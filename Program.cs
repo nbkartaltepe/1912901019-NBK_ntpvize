44ac6dd2bcd49ba7e4161019dd4428699751a62e
Naci Berkay Kartaltepe <nbkcsgoek@hotmail.com>
2021-04-25 14:05:40 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1912901019_NBK_ntpvize
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
